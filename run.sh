d7579a27a05a771843a367dd8ac91d896b6300b8
Geordan Liban <geordan@users.noreply.github.com>
2021-06-25 23:38:39 -0700

    fix container exiting after command returns non-zero exit code

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-# docker run -v 'C:\Users\P1GXL02:/home/p1gxl02' -it geordandev:0.0.1 /bin/bash
+
+set -eu
+
+echo ""
+echo "Run '. ./setup.sh && source .bashrc'"
+echo ""
+
 docker run \
   -v '/Users/geordan/code/geordan/dotfiles:/dotfiles' \
   -v '/Users/geordan/code:/home/dev/code' \
--- setup.sh
+++ setup.sh
@@ -3,5 +3,3 @@
 set -eu
 
 for f in $(ls -A /dotfiles | grep -v README | grep -vE '^.git/*$' ); do ln -s /dotfiles/$f $f; done
-
-source .bashrc
