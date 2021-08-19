170ca25cbef6df6d482f78d96a8a96462dbf2687
Geordan Liban <geordan@users.noreply.github.com>
2021-08-19 13:22:21 -0700

    use --env-file, don't strip spaces from build-arg

--- run.py
+++ run.py
@@ -14,14 +14,14 @@ with open("config.py", "r") as config_file:
     for line in config_file:
         if line.startswith("#"):
             continue
-        build_arg_str += line.strip()
+        build_arg_str += line.strip('\n')
         build_arg_str += " --build-arg "
 
 # remove last ' --build-arg'... hacky
-build_arg_str = "".join(build_arg_str.split()[:-1])
+build_arg_str = " ".join(build_arg_str.split()[:-1])
 
-env_arg_str = build_arg_str.replace("--build-arg", "--env")
-env_arg_str = env_arg_str.replace("'", "")
+# env_arg_str = build_arg_str.replace("--build-arg", "--env")
+# env_arg_str = env_arg_str.replace("'", "")
 
 # print(build_arg_str)
 # print(env_arg_str)
@@ -43,8 +43,8 @@ run_result = subprocess.run(
     [
         "docker",
         "run",
-        "--env",
-        env_arg_str,
+        "--env-file",
+        "config.py",
         "-v",
         f"{home}/code/dotfiles:/dotfiles",
         "-v",
